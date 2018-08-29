0507a9b633e4a73a43c57c888635ff1cc1ed8bcf
Rohit Patnaik <rohit@terbiumlabs.com>
2018-08-29 11:01:19 -0500

    Convert script to operate on filenames
    This makes the script easier to use with `find`

--- four_to_two.py
+++ four_to_two.py
@@ -15,11 +15,21 @@ def four_to_two(line):
         beginning_space_count = len(match.group(1))
         indent_level = math.floor(beginning_space_count / 4)
         new_indent = '  ' * indent_level
-        print("{}{}".format(new_indent, match.group(2)))
+        return "{}{}".format(new_indent, match.group(2))
     else:
-        print(line.strip())
+        return line.strip()
+
+def print_usage_and_exit():
+    print("Usage: four_to_two.py <input_file> <output_file>")
+    sys.exit(1)
 
 if __name__ == "__main__":
-    for line in sys.stdin:
-        four_to_two(line)
-            
+    if len(sys.argv) < 3:
+        print_usage_and_exit()
+    with open(sys.argv[1], 'r') as input_file:
+        input_lines = input_file.readlines()
+        with open(sys.argv[2], 'w') as output_file:
+            for line in input_lines:
+                output_file.write("{}\n".format(four_to_two(line)))
+            output_file.flush()
+
